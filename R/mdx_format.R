2a990e85a340759c39d2f07ac09e7dd4db39d0f7
McDonnell, Robert <robertmylesmcdonnell@protonmail.com>
2020-02-29 13:13:11 +0000

    update markdown format

--- R/mdx_format.R
+++ R/mdx_format.R
@@ -1,31 +1,63 @@
-mdx_format = function(variant = "markdown_strict",
-                      preserve_yaml = TRUE,
-                      dev = 'png',
-                      df_print = "tibble",
-                      fig_width = 7,
-                      fig_height = 5){
+mdx_format <- function(variant = "markdown", preserve_yaml = TRUE,
+                       dev = "png", df_print = "tibble", fig_width = 7,
+                       fig_height = 5, cfig) {
   args <- ""
 
   # add post_processor for yaml preservation
-  post_processor <- if (preserve_yaml) {
-    function(metadata, input_file, output_file, clean, verbose) {
+  post_processor <- function(metadata, input_file, output_file, clean, verbose) {
       input_lines <- read_utf8(input_file)
       partitioned <- partition_yaml_front_matter(input_lines)
+
+      if (length(cfig) > 0) {
+        yaml_headers <- partitioned$front_matter
+        last <- length(yaml_headers)
+        yaml_headers <- yaml_headers[-last]
+        yaml_headers <- yaml_headers[-1]
+        yaml_fields <- unlist(cfig)
+
+        for (i in 1:length(cfig)) {
+          param <- yaml_fields[[i]]
+          present <- grepl(yaml_fields[[i]], yaml_headers[[i]])
+          if (!present) {
+            stop(paste0("Config item '", param, "' is missing from YAML."))
+          } else if (param == "date") {
+            date_string <- strex::str_after_first(yaml_headers[[i]], "date: ")
+            date_string <- gsub(x = date_string, pattern = '"', replacement = "")
+            first <- strex::str_before_first(date_string, "/")
+            if (nchar(first) < 2) first <- paste0("0", first)
+            second <- strex::str_after_first(date_string, "/")
+            third <- strex::str_after_last(second, "/")
+            second <- strex::str_before_first(second, "/")
+            if (nchar(second) < 2) second <- paste0("0", second)
+            # account for RStudio default RMarkdown creating dates as "2/28/2020"
+            date_out <- paste0(third, "-", first, "-", second)
+            date_position <- which(grepl("date", partitioned$front_matter))
+            yaml_date_out <- paste0("date: '", date_out, "'")
+            partitioned$front_matter[date_position] <- yaml_date_out
+
+          }
+        }
+        if (length(yaml_headers) > length(yaml_fields)) {
+          message("You have more items in YAML headers than your config. Just to let you know.")
+        }
+      }
+
       if (!is.null(partitioned$front_matter)) {
         output_lines <- c(partitioned$front_matter, "", read_utf8(output_file))
         write_utf8(output_lines, output_file)
       }
       output_file
     }
-  }
 
   # return format
   rmarkdown:::output_format(
     knitr = rmarkdown:::knitr_options_html(fig_width, fig_height, fig_retina = NULL, FALSE, dev),
-    pandoc = rmarkdown:::pandoc_options(to = variant,
-                            from = rmarkdown::from_rmarkdown(),
-                            args = args,
-                            ext = '.mdx'),
+    pandoc = rmarkdown:::pandoc_options(
+      to = variant,
+      from = rmarkdown::from_rmarkdown(),
+      args = args,
+      ext = ".mdx"
+    ),
     keep_md = FALSE,
     clean_supporting = FALSE,
     df_print = df_print,
--- R/writeMDX.R
+++ R/writeMDX.R
@@ -2,12 +2,14 @@
 #' @title writeMDX
 #' @description write and Rmarkdown (.Rmd) file to MDX (.mdx) format.
 #' @param input Input file (.Rmd format).
+#' @param config \code{list}. Configurable YAML fields to check for. Default is
+#' config = list("date", "title", "featuredImage")
 #' @examples
 #' \dontrun{
 #' # supposing you have a 'test.Rmd' file:
 #' writeMDX("test.Rmd")
 #' }
 #' @export
-writeMDX <- function(input){
-  rmarkdown::render(input, mdx_format())
+writeMDX <- function(input, config = list("date", "title", "featuredImage")){
+  rmarkdown::render(input, mdx_format(cfig = config))
 }
